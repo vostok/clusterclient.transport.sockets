fd76abef78b62468993a3fe3ad567c00fd4740db
epeshk <peshkov@skbkontur.ru>
2018-09-18 11:55:20 +0500

    add ProxyTests

---
+++ Vostok.ClusterClient.Transport.Sockets.Tests/Functional/ProxyTests.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Net;
+using FluentAssertions;
+using NUnit.Framework;
+using Vostok.ClusterClient.Core.Model;
+using Vostok.ClusterClient.Transport.Sockets.Tests.Functional.Helpers;
+
+namespace Vostok.ClusterClient.Transport.Sockets.Tests.Functional
+{
+    internal class ProxyTests : TransportTestsBase
+    {
+        [Test]
+        public void Should_send_request_to_proxy_if_setting_is_true()
+        {
+            using (var proxy = TestServer.StartNew(ctx => ctx.Response.StatusCode = 201))
+            {
+                SetSettings(s => s.Proxy = new WebProxy(proxy.Url, false));
+
+                var expectedUrl = new Uri($"http://vostok:{proxy.Port}");
+                
+                var request = Request.Get("http://vostok");
+                var response = Send(request);
+                response.Code.Should().Be(ResponseCode.Created);
+                proxy.LastRequest.Url.Should().Be(expectedUrl);
+            }
+        }
+    }
+}
