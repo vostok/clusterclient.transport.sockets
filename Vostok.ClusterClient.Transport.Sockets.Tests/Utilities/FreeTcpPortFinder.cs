2fcc5f1853926c5561ca3e57c42fc9acdbfd608f
epeshk <peshkov@skbkontur.ru>
2018-09-18 14:53:08 +0500

    split to files

---
+++ Vostok.ClusterClient.Transport.Sockets.Tests/Utilities/FreeTcpPortFinder.cs
@@ -0,0 +1,22 @@
+using System.Net;
+using System.Net.Sockets;
+
+namespace Vostok.ClusterClient.Transport.Sockets.Tests.Utilities
+{
+    internal static class FreeTcpPortFinder
+    {
+        public static int GetFreePort()
+        {
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            try
+            {
+                listener.Start();
+                return ((IPEndPoint)listener.LocalEndpoint).Port;
+            }
+            finally
+            {
+                listener.Stop();
+            }
+        }
+    }
+}
