ece7a712415bcfabb68acd33ee2e825da9a5bc00
epeshk <peshkov@skbkontur.ru>
2018-09-25 14:46:53 +0500

    remove old tests

--- Vostok.ClusterClient.Transport.Sockets/SocketsTransport.cs
+++ Vostok.ClusterClient.Transport.Sockets/SocketsTransport.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Net.Sockets;
-using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Vostok.ClusterClient.Core.Model;
