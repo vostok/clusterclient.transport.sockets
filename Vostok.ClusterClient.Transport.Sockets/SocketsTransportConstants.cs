bacf5634aba6b81b67ad6da9a6e21c9004bf258e
epeshk <peshkov@skbkontur.ru>
2018-10-19 15:25:58 +0500

    use 16 kb buffer

--- Vostok.ClusterClient.Transport.Sockets/SocketsTransportConstants.cs
+++ Vostok.ClusterClient.Transport.Sockets/SocketsTransportConstants.cs
@@ -2,7 +2,7 @@ namespace Vostok.Clusterclient.Transport.Sockets
 {
     internal class SocketsTransportConstants
     {
-        public const int PooledBufferSize = 64*1024;
+        public const int PooledBufferSize = 16*1024;
         public const int PreferredReadSize = PooledBufferSize;
         public const int LOHObjectSizeThreshold = 85*1000;
     }
