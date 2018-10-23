4d62b2c6a643c12329b2d25442e674fae7f3f853
epeshk <peshkov@skbkontur.ru>
2018-10-23 13:03:41 +0500

    add xml comments

--- Vostok.ClusterClient.Transport.Sockets/SocketsTransportSettings.cs
+++ Vostok.ClusterClient.Transport.Sockets/SocketsTransportSettings.cs
@@ -4,62 +4,83 @@ using System.Net.Http;
 
 namespace Vostok.Clusterclient.Transport.Sockets
 {
+    /// <summary>
+    /// A class that represents <see cref="SocketsTransport"/> settings.
+    /// </summary>
     public class SocketsTransportSettings
     {
         /// <summary>
-        ///     An attempts count to establish TCP connection with target host.
+        /// Gets or sets an attempts count to establish TCP connection with target host.
         /// </summary>
         public int ConnectionAttempts { get; set; } = 2;
 
         /// <summary>
-        ///     How much time connection will be alive after last usage. Note that if none other connections to endpoint is active,
-        ///     ConnectionIdleTimeout will be divided by 4.
+        /// How much time connection will be alive after last usage. Note that if none other connections to endpoint is active,
+        /// ConnectionIdleTimeout will be divided by 4.
         /// </summary>
         public TimeSpan ConnectionIdleTimeout { get; set; } = TimeSpan.FromMinutes(2);
 
         /// <summary>
-        ///     How much time client should wait for internal handler return after request cancellation.
+        /// How much time client should wait for internal handler return after request cancellation.
         /// </summary>
         public TimeSpan RequestAbortTimeout { get; set; } = TimeSpan.FromMilliseconds(250);
 
         /// <summary>
-        ///     A <see cref="IWebProxy" /> instance which will be used to send requests.
+        /// Gets or sets a <see cref="IWebProxy" /> instance which will be used to send requests.
         /// </summary>
         public IWebProxy Proxy { get; set; }
 
         /// <summary>
-        ///     Max connections count to single endpoint. When this limit is reached, request falls into queue and wait for free
-        ///     connection.
+        /// Max connections count to single endpoint. When this limit is reached, request falls into queue and wait for free
+        /// connection.
         /// </summary>
         public int MaxConnectionsPerEndpoint { get; set; } = 10 * 1000;
 
         /// <summary>
-        ///     Max response body size in bytes. This parameter doesn't affect on content streaming.
+        /// Gets or sets the maximum response body size in bytes. This parameter doesn't affect on content streaming.
         /// </summary>
         public long? MaxResponseBodySize { get; set; }
 
+        /// <summary>
+        /// Gets or sets the maximum amount of data that can be drained from responses in bytes.
+        /// </summary>
         public int? MaxResponseDrainSize { get; set; }
 
+        /// <summary>
+        /// Gets or sets the delegate that decide use response streaming or not.
+        /// </summary>
         public Predicate<long?> UseResponseStreaming { get; set; } = _ => false;
 
+        /// <summary>
+        /// Gets or sets a value that indicates whether the transport should follow HTTP redirection responses.
+        /// </summary>
         public bool AllowAutoRedirect { get; set; }
 
         public TimeSpan ConnectionLifetime { get; set; } = TimeSpan.FromSeconds(3);
 
+        /// <summary>
+        /// Gets or sets a custom delegate that can be used to tune underlying <see cref="SocketsHttpHandler"/>.
+        /// </summary>
         public Action<SocketsHttpHandler> Tune { get; set; }
 
+        /// <summary>
+        /// Enables TCP keep alive.
+        /// </summary>
         public bool TcpKeepAliveEnabled { get; set; }
 
+        /// <summary>
+        /// Enables ARP cache warmup
+        /// </summary>
         public bool ArpCacheWarmupEnabled { get; set; } = false;
 
         /// <summary>
-        ///     The duration between two keepalive transmissions in idle condition.
+        /// Gets or sets the duration between two keepalive transmissions in idle condition.
         /// </summary>
         public TimeSpan TcpKeepAliveTime { get; set; } = TimeSpan.FromSeconds(3);
 
         /// <summary>
-        ///     The duration between two successive keepalive retransmissions, if acknowledgement to the previous keepalive
-        ///     transmission is not received.
+        /// Gets ot sets the duration between two successive keepalive retransmissions, if acknowledgement to the previous keepalive
+        /// transmission is not received.
         /// </summary>
         public TimeSpan TcpKeepAliveInterval { get; set; } = TimeSpan.FromSeconds(1);
 
