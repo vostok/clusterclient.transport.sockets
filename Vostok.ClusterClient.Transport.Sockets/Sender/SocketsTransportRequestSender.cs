eaa99ec0eb3c5934bb9179d53ea2a7a670b55ea7
epeshk <peshkov@skbkontur.ru>
2018-10-24 00:57:39 +0500

    use structured logging

--- Vostok.ClusterClient.Transport.Sockets/Sender/SocketsTransportRequestSender.cs
+++ Vostok.ClusterClient.Transport.Sockets/Sender/SocketsTransportRequestSender.cs
@@ -95,8 +95,8 @@ namespace Vostok.Clusterclient.Transport.Sockets.Sender
             }
             catch (HttpRequestException e) when (IsConnectionFailure(e, cancellationToken))
             {
-                var message = $"Connection failure. Target = {request.Url.Authority}.";
-                log.Warn(e, message);
+                var message = "Connection failure. Target = {Target}.";
+                log.Warn(e, message, request.Url.Authority);
                 return Responses.ConnectFailure;
             }
 
