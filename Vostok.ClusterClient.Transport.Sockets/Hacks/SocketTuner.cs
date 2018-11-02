eee98f12939e829046afc859de11cb48fdd12241
epeshk <peshkov@skbkontur.ru>
2018-11-02 15:22:45 +0500

    move ArpCacheMaintainer to vostok.commons.helpers

--- Vostok.ClusterClient.Transport.Sockets/Hacks/SocketTuner.cs
+++ Vostok.ClusterClient.Transport.Sockets/Hacks/SocketTuner.cs
@@ -2,7 +2,7 @@ using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
-using Vostok.Clusterclient.Transport.Sockets.ArpCache;
+using Vostok.Commons.Helpers.Network;
 using Vostok.Logging.Abstractions;
 
 namespace Vostok.Clusterclient.Transport.Sockets.Hacks
