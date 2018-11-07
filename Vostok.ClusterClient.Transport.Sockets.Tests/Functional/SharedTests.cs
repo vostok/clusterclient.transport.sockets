fe91f52da18e80db547c33ad2b5929cef4e12977
epeshk <peshkov@skbkontur.ru>
2018-11-07 17:20:57 +0500

    add test for streaming

--- Vostok.ClusterClient.Transport.Sockets.Tests/Functional/SharedTests.cs
+++ Vostok.ClusterClient.Transport.Sockets.Tests/Functional/SharedTests.cs
@@ -73,4 +73,7 @@ namespace Vostok.Clusterclient.Transport.Sockets.Tests.Functional
     internal class StatusCodeReceivingTests : StatusCodeReceivingTests<Config>
     {
     }
+    internal class ContentStreamingTests : ContentStreamingTests<Config>
+    {
+    }
 }
