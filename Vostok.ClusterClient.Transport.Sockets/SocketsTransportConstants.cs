287080a8c97187a42e19d1361f0ea59c4850ce18
iloktionov <iloktionov@skbkontur.ru>
2019-01-19 18:37:18 +0500

    SocketsTransportConstants: made the class static.

--- Vostok.ClusterClient.Transport.Sockets/SocketsTransportConstants.cs
+++ Vostok.ClusterClient.Transport.Sockets/SocketsTransportConstants.cs
@@ -1,6 +1,6 @@
 namespace Vostok.Clusterclient.Transport.Sockets
 {
-    internal class SocketsTransportConstants
+    internal static class SocketsTransportConstants
     {
         public const int PooledBufferSize = 16 * 1024;
         public const int PreferredReadSize = PooledBufferSize;
